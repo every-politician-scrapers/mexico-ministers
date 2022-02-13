520e728bfcc562a44f5f4d3c4126a61698d0d257
Tony Bowden <tony@tmtm.com>
2022-02-13 15:50:12 +0200

    compare eswiki Ambassadors to the UK

---
+++ sources/ambassador-uk/scraper.rb
@@ -0,0 +1,33 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator ReplaceZeroWidthSpaces
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Título'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name title start _ end].freeze
+    end
+
+    def raw_end
+      super.gsub('*', '').tidy
+    end
+
+    def empty?
+      tds[0].text == tds[1].text
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
