f3371f6392f7085f2fb8bc2925051884a81a8186
Tony Bowden <tony@tmtm.com>
2022-02-17 14:12:40 +0200

    compare eswiki Deputies of the 54th Congress

---
+++ sources/deputies-54/scraper.rb
@@ -0,0 +1,37 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def holder_entries
+    noko.xpath("//table[.//th[contains(.,'Diputado')]][position()=3]//tr[td]//td[position()=3 or position()=7]//a") +
+    noko.xpath("//table[.//th[contains(.,'Diputado')]][position()=4]//tr[td]//td[position()=2 or position()=5]//a")
+  end
+
+  class Officeholder < OfficeholderBase
+    def itemLabel
+      noko.text.tidy
+    end
+
+    def item
+      noko.attr('wikidata')
+    end
+
+    def startDate
+      '2018-09-01'
+    end
+
+    def endDate
+      '2021-08-31'
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
